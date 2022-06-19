28ae333cf2fc4acbb0b576e64595636b86acf51b
Andrew Witwicki <vitvitskiy3@gmail.com>
2022-06-19 13:39:26 +0200

    Initial code

---
+++ RosirScrapper/HtmlScrapper.cs
@@ -0,0 +1,34 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosirScrapper
+{
+    public static class HtmlScrapper
+    {
+        public static async Task<string> GetTopOfTheDayPhotoUrl(string url)
+        {
+            // Request page
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument htmlDoc = await web.LoadFromWebAsync(url);
+
+            // Get data
+            HtmlNode data = htmlDoc
+                .DocumentNode
+                .SelectSingleNode(@"//h4[contains(@class, 'mb-0')]");
+
+            if (data != null)
+            {
+                //Obecnie na obiekcie przebywa: 2250 osób
+                string result = data.InnerText.Replace("Obecnie na obiekcie przebywa: ", "").Replace(" osób", "");
+
+                return result;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ RosirScrapper/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RosirScrapper;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    try
+    {
+        var peopleCount = await HtmlScrapper.GetTopOfTheDayPhotoUrl("https://rosir.nq.pl/");
+       
+        if (peopleCount != null)
+        {
+            Console.WriteLine(peopleCount);
+
+            // Send to influx
+            string query = $"iot,room=rosir,device=api,sensor=count_people {peopleCount}";
+
+            new Task(() =>
+            {
+                try
+                {
+                    using (var client = new System.Net.Http.HttpClient())
+                    {
+                        client.BaseAddress = new Uri("http://localhost:8086/write?db=bots");
+                        client.Timeout = TimeSpan.FromSeconds(1);
+                        var content = new System.Net.Http.StringContent(query, Encoding.UTF8, "application/Text");
+                        var res = client.PostAsync("", content).Result;
+                        var tt = res.Content.ReadAsStringAsync().Result;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"query: {query}");
+                    Console.WriteLine(ex);
+                }
+            }).Start();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+
+    await Task.Delay(30000);
+}
